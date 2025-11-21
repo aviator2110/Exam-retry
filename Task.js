0e70eb452ae4abf4a651fd5c37853311f8e7e35f
Guram Gognidze Work <quram21@gmail.com>
2025-11-21 11:40:58 +0400

    Task ctr, getters setters, toJSON. TM ctr load and save

--- Task.js
+++ Task.js
@@ -1,14 +1,67 @@
 export class Task {
+    static #lastId = 0;
     #id;
     #name;
     #description;
     #creationDate;
     #isComplete;
 
-    constructor(task) {}
+    constructor(name, description, id, creationDate, isComplete) {
+        if (arguments.length === 2) {
+            this.#id = Task.#lastId++;
+            this.#name = name;
+            this.#description = description;
+            this.#creationDate = new Date().toISOString();
+            this.#isComplete = false;
+        } else {
+            this.#id = id;
+            this.#name = name;
+            this.#description = description;
+            this.#creationDate = creationDate;
+            this.#isComplete = isComplete;
+        }
+    }
 
     deleteTask() {
 
+    }
+
+    get id(){
+        return this.#id;
+    }
+
+    get name(){
+        return this.#name;
+    }
+
+    get description(){
+        return this.#description;
+    }
+
+    get creationDate(){
+        return this.#creationDate;
+    }
+
+    get isComplete(){
+        return this.#isComplete;
+    }
+
+    static get lastId(){
+        return Task.#lastId;
+    }
+
+    static set lastId(lastId) {
+        Task.#lastId = lastId;
+    }
+
+    toJSON(){
+        return {
+            id: this.#id,
+            name: this.#name,
+            description: this.#description,
+            creationDate: this.#creationDate,
+            isComplete: this.#isComplete,
+        }
     }
 
     editTask() {
--- TaskManager.js
+++ TaskManager.js
@@ -1,13 +1,45 @@
+import { Task } from "./Task.js";
+
 export class TaskManager {
-    #allTasks;
+    #allTasks = [];
 
-    constructor() {}
+    constructor() {
+        this.#loadTasks();
+    }
 
-    addTask() {
+    addTask(name, description) {
+        const task = new Task(name, description);
+        this.#allTasks.push(task);
 
+        this.#saveTasks()
     }
 
     deleteTask() {
 
+    }
+
+    #loadTasks(){
+        const raw = JSON.parse(localStorage.getItem("tasks")) || [];
+
+        const tasks = raw.map(obj =>
+            new Task(obj.name, obj.description, obj.id, obj.creationDate, obj.isComplete)
+        );
+
+        let maxId = tasks[0].id;
+
+        for (let i = 0; i < tasks.length; i++) {
+            if (tasks[i].id > maxId) {
+                maxId = tasks[i].id;
+            }
+        }
+
+        Task.lastId = maxId;
+
+        return tasks;
+    }
+
+    #saveTasks(){
+        const json = JSON.stringify(this.#allTasks);
+        localStorage.setItem("tasks", json);
     }
 }
