ca9f9210ea8be3d9c74e540daed8bbd303b64859
Guram Gognidze Work <quram21@gmail.com>
2025-11-21 17:43:44 +0400

    Style for 404 page

--- edit/edit.js
+++ edit/edit.js
@@ -11,18 +11,24 @@ console.log("Editing task with id:", id);
 const tasks = manager.allTasks;
 const task = tasks.find(t => t.id === id);
 
+const notFound404 = document.getElementById("not-found");
+const editForm = document.getElementById("edit-form");
+
+
 if (!task) {
-    alert("Task not found");
+    notFound404.style.display = "flex";
+    notFound404.style.flexDirection = "column";
+    editForm.style.display = "none";
+}else {
+    document.getElementById("name").value = task.name;
+    document.getElementById("description").value = task.description;
+
+    document.getElementById("save").addEventListener("click", () => {
+        task.name = document.getElementById("name").value;
+        task.description = document.getElementById("description").value;
+
+        manager.saveTasks;
+
+        window.location.href = "../index.html";
+    });
 }
-
-document.getElementById("name").value = task.name;
-document.getElementById("description").value = task.description;
-
-document.getElementById("save").addEventListener("click", () => {
-    task.name = document.getElementById("name").value;
-    task.description = document.getElementById("description").value;
-
-    manager.saveTasks;
-
-    window.location.href = "../index.html";
-});
